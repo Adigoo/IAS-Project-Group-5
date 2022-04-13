860af0205214b538ce0ad9ca512d5150e3442cac
MaheshCodeReader <mdudhnale@gmail.com>
2022-04-13 18:00:01 +0530

    docker code

---
+++ sensor_manager/client.py
@@ -0,0 +1,15 @@
+import logging
+from urllib import response
+from itsdangerous import json
+import requests
+
+def getData(topic_name):
+    localhost_ip_address = "172.17.0.1"
+    # pub_ip = requests.get("http://api.ipify.org").content.decode()
+    # localhost_ip_address = pub_ip
+    response = requests.post("http://{localhost_ip_address}:5000/getSensorData", json={
+        "topic_name": topic_name
+    })
+    logging.warning(response.json)
+
+getData()
