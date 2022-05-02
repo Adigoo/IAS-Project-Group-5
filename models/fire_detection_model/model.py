e0bea0425ae5f1e22cbedd8c9019e9edb5f2465c
Jasika16 <jasikashah1610@gmail.com>
2022-05-03 02:44:22 +0530

    update fire detection model

--- models/fire_detection_model/model.py
+++ models/fire_detection_model/model.py
@@ -15,60 +15,72 @@ import pickle
 import warnings
 import numpy as np
 warnings.filterwarnings("ignore")
+class FireDetection:
+  def __init__(self):
+    self.temp_data, self.fire_alarm_data = self.generate_data()
+    self.input_data = self.create_Df( )
+    self.X_train, self.X_test, self.y_train, self.y_test = self.split_data(  )
+    self.decision_tree_model = self.Decison_Tree()
 
-def generate_data():
+  def generate_data(self):
 
-  # generate temperature data
-  temp_data = []
-  for i in range( 10000 ):
-    temp = random.uniform(10, 50)
-    temp_data.append( temp )
+    # generate temperature data
+    temp_data = []
+    for i in range( 10000 ):
+      temp = random.uniform(10, 50)
+      temp_data.append( temp )
 
-  # generate fire_alarm data
-  fire_alarm_data = []
-  for i in range( 10000 ):
-    fire_alarm = random.randint(0,1)
-    if fire_alarm == 0:
-      fire_alarm_data.append( False )
-    else:
-      fire_alarm_data.append( True )
+    # generate fire_alarm data
+    fire_alarm_data = []
+    for i in range( 10000 ):
+      fire_alarm = random.randint(0,1)
+      if fire_alarm == 0:
+        fire_alarm_data.append( False )
+      else:
+        fire_alarm_data.append( True )
 
-  return temp_data, fire_alarm_data
+    return temp_data, fire_alarm_data
 
-temp_data, fire_alarm_data = generate_data()
-
-def create_Df( temp_data, fire_alarm_data ):
-  input_data = pd.DataFrame(list(zip(temp_data, fire_alarm_data)), columns=['Temperature', 'Fire_Alarm'])
-  return input_data
-
-input_data = create_Df( temp_data, fire_alarm_data )
-
-input_data.head()
-
-def split_data( input_data ):
-  #X = input_data.drop(columns="Fire_Alarm")
-  X = input_data["Temperature"]
   
-  y = input_data["Fire_Alarm"]
+
+  def create_Df(self):
+    input_data = pd.DataFrame(list(zip(self.temp_data, self.fire_alarm_data)), columns=['Temperature', 'Fire_Alarm'])
+    return input_data
+
   
-  X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.80)
-  return X_train, X_test, y_train, y_test
 
-X_train, X_test, y_train, y_test = split_data( input_data )
-print( X_train )
-print( y_train )
+  def split_data(self ):
+    #X = input_data.drop(columns="Fire_Alarm")
+    X = self.input_data["Temperature"]
+    
+    y = self.input_data["Fire_Alarm"]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.80)
+    return X_train, X_test, y_train, y_test
 
-def Decison_Tree( X_train ):
-  X_train = np.array( X_train )
-  X_train = X_train.reshape(-1, 1)
-  decision_tree_model = DecisionTreeClassifier(max_depth =3, random_state = 42)
-  decision_tree_model.fit(X_train, y_train)
-  return decision_tree_model
+  
 
-decision_tree_model = Decison_Tree(X_train)
+  def Decison_Tree( self ):
+    self.X_train = np.array( self.X_train )
+    self.X_train = self.X_train.reshape(-1, 1)
+    decision_tree_model = DecisionTreeClassifier(max_depth =3, random_state = 42)
+    decision_tree_model.fit(self.X_train, self.y_train)
+    return decision_tree_model
 
-pickle_file = open('model.pkl', 'ab')
-pickle.dump( decision_tree_model, pickle_file )
+  def predict(self, data):
+    result =self.decision_tree_model.predict([[data]])
+    return result[0]
+
+
+obj = FireDetection()
+
+pickle_file = open('model.pkl', 'wb')
+pickle.dump( obj, pickle_file )
 pickle_file.close()
 
+
+file = open("model.pkl", "rb")
+x = pickle.load(file)
+
+#print(x.predict(47.857214))
 #decision_tree_model.predict([[47.857214]])
